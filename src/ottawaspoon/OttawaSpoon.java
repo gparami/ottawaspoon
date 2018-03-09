1bbc0cf26d3f99eddc1bc8bf7917665cbf7995dd
artemtar <artemtarasov89@gmail.com>
2018-03-08 23:15:04 -0500

    Initial state, all set

--- src/ottawaspoon/OttawaSpoon.java
+++ src/ottawaspoon/OttawaSpoon.java
@@ -1,8 +1,18 @@
 package ottawaspoon;
 
+import java.sql.*;
+
 public class OttawaSpoon {
 
 	public static void main(String[] args) {
 		System.out.println("You suck!!! Thank you.");
+		try {
+            Class.forName("org.postgresql.Driver");
+            String url = "jdbc:postgres://tpjdrpaw:ojYNNlKphTrospph3Evc9gS4OMv9N9RZ@stampy.db.elephantsql.com:5432/tpjdrpaw";
+            Connection db = DriverManager.getConnection(url, "tpjdrpaw", "ojYNNlKphTrospph3Evc9gS4OMv9N9RZ");
+            Statement st = db.createStatement();
+            st.close();
+        } catch (Exception e){System.out.println("Error - erorr@!!!!2211!@!@!@!!");}
+		
 	}
 }
