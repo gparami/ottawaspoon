e200a3af31a4b6e747f3c5c64ba4813d893c6935
artemtar <artemtarasov89@gmail.com>
2018-03-11 19:23:20 -0400

    added queries file and password + id should be loaded from separate file later

--- src/ottawaspoon/OttawaSpoon.java
+++ src/ottawaspoon/OttawaSpoon.java
@@ -9,7 +9,9 @@ public class OttawaSpoon {
 		try {
             Class.forName("org.postgresql.Driver");
             String url = "jdbc:postgres://tpjdrpaw:ojYNNlKphTrospph3Evc9gS4OMv9N9RZ@stampy.db.elephantsql.com:5432/tpjdrpaw";
-            Connection db = DriverManager.getConnection(url, "tpjdrpaw", "ojYNNlKphTrospph3Evc9gS4OMv9N9RZ");
+            String userid = "tpjdrpaw";
+            String password = "ojYNNlKphTrospph3Evc9gS4OMv9N9RZ";
+            Connection db = DriverManager.getConnection(url, userid, password);
             Statement st = db.createStatement();
             st.close();
         } catch (Exception e){System.out.println("Error - erorr@!!!!2211!@!@!@!!"); System.out.println(e);}
