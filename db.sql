ca3aadb1f9f53df54042ff4783d8b288bdc84cf1
Parami Gunasekara <parami@live.ca>
2018-03-16 12:50:27 -0400

    added database and schema creation
    Also changed the schema to ottawaspoon instead of public.

--- db.sql
+++ db.sql
@@ -1,11 +1,19 @@
+/*
+Database is created with the name ottawaspoondb and the schema ottawaspoon is created for application usage.
+If connecting to a existing database, start your sql script from line 7.
+*/
+CREATE DATABASE ottawaspoondb;
+\connect ottawaspoondb;
+CREATE SCHEMA ottawaspoon;
 set search_path = 'ottawaspoon';
+
 /*
 The joinate is used to show when this rater first joined the website. The name field corresponds to
 an alias such as SuperSizeMe. Type refers to the type of rater (blog, online, food critic) and
 reputation takes a value between 1 and 5. The value of this field is based on the number of people
 who found this rater opinion helpful, and the default value is 1 (lowest).
 */
-create table public.rater(
+create table ottawaspoon.rater(
     userID int, 
     name varchar, 
     type int, 
@@ -15,7 +23,7 @@ create table public.rater(
     primary key (userid)
     );
 
-alter table public.rater
+alter table ottawaspoon.rater
 add constraint rater_type
     check (type between 1 and 5);
 /*
@@ -23,7 +31,7 @@ This relation contains general information about a restaurant and is useful in t
 restaurant chain has many locations. The type attribute contains details about the cuisine, such as
 Italian, Indian, Middle Eastern, and so on.
 */
-create table public.restaurant(
+create table ottawaspoon.restaurant(
     restaurantID int, 
     name varchar,
     type varchar,
@@ -39,7 +47,7 @@ The Price, Food, Mood and Staff attributes may take a value between 1 (low) to 5
 comments field is reserved for free text and will be used, in future, for sentiment analysis. Note
 that UserID and RestaurantID are foreign keys
 */
-create table public.rating(
+create table ottawaspoon.rating(
     userID int,
     date date, 
     price int, 
@@ -54,15 +62,15 @@ create table public.rating(
 
     primary key (userid, date),
 
-    foreign key (userID) references public.rater (userID),
-    foreign key (restaurantID) references public.restaurant (restaurantID)
+    foreign key (userID) references ottawaspoon.rater (userID),
+    foreign key (restaurantID) references ottawaspoon.restaurant (restaurantID)
     );
 /*
 This relation contains the locationpecific data, such as the managers details, the phone number,
 the address, and so on. Note that RestaurantID is the foreign key. This design assumes that the
 restaurant opens and closes at the same time every day; you may modify this design if you wish
 */
-create table public.location(
+create table ottawaspoon.location(
     locationID int, 
     open_date date,
     manager varchar, 
@@ -74,14 +82,14 @@ create table public.location(
 
     primary key (locationid),
 
-    foreign key (restaurantId) references public.restaurant (restaurantId)
+    foreign key (restaurantId) references ottawaspoon.restaurant (restaurantId)
     );
 
 /*
     Here we include the item name, as on the menu, the category (starter, main, desert) as well as the
 type (food or beverage). RestaurantID is the foreign key.
 */
-create table public.menuitem(
+create table ottawaspoon.menuitem(
     itemID int, 
     name varchar,
     type varchar, 
@@ -92,14 +100,14 @@ create table public.menuitem(
 
     primary key (itemid),
 
-    foreign key (restaurantID) references public.restaurant (restaurantID)
+    foreign key (restaurantID) references ottawaspoon.restaurant (restaurantID)
     );
 	
 /*
 A rater may explicitly select the menu item, and add a specific rating between 1 (low) to 5 (high)
 and a free text comment. All menu items should be selected from a list.
 */
-create table public.ratingItem(
+create table ottawaspoon.ratingItem(
     userID int, 
     date date, 
     itemID int, 
@@ -108,8 +116,8 @@ create table public.ratingItem(
 
     primary key (userid,date,itemid),
 	
-    foreign key (userID,date) references public.rating (userID,date),
-    foreign key (itemID) references public.menuitem (itemID)
+    foreign key (userID,date) references ottawaspoon.rating (userID,date),
+    foreign key (itemID) references ottawaspoon.menuitem (itemID)
     );
 
 
