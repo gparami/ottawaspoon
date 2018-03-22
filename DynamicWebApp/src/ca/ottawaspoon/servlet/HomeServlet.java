69db1af5046a0855cbe28dee8dab0c9216869d6e
Parami Gunasekara <parami@live.ca>
2018-03-22 11:41:33 -0400

    homeServlet

--- DynamicWebApp/src/ca/ottawaspoon/servlet/HomeServlet.java
+++ DynamicWebApp/src/ca/ottawaspoon/servlet/HomeServlet.java
@@ -22,11 +22,9 @@ public class HomeServlet extends HttpServlet {
 	   protected void doGet(HttpServletRequest request, HttpServletResponse response)
 	           throws ServletException, IOException {
 	 
-	        
 	       // Forward to /WEB-INF/views/homeView.jsp
 	       // (Users can not access directly into JSP pages placed in WEB-INF)
 	       RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/WEB-INF/views/homeView.jsp");
-	        
 	       dispatcher.forward(request, response);
 	        
 	   }
