2f4cf9e9c5474e98aea420a17e5834834f92e903
Aleks048 <ashum048@uottawa.ca>
2018-03-26 14:59:18 -0400

    hi

---
+++ insert_delete_update.sql
@@ -0,0 +1,9 @@
+--insert the data into the rater/restaurant/menuItem
+insert into ottawaspoon.rater (userId,e_mail,name,type,join_date,reputation,password) values,
+
+insert into ottawaspoon.restaurant(restaurantID,name,type,url) values,
+
+insert into ottawaspoon.MenuItem(itemID,name,type,category,description,price,restaurantID) values,
+
+--delete the data into the rater/restaurant/menuItem
+
