eb8c855ee859bce370a1980d10a31a664236fc2a
Aleks048 <ashum048@uottawa.ca>
2018-03-26 15:43:12 -0400

    para quiries

--- insert_delete_update.sql
+++ insert_delete_update.sql
@@ -1,9 +1,15 @@
 --insert the data into the rater/restaurant/menuItem
-insert into ottawaspoon.rater (userId,e_mail,name,type,join_date,reputation,password) values,
+insert into ottawaspoon.rater (userId,e_mail,name,type,join_date,reputation,password) values,()
 
-insert into ottawaspoon.restaurant(restaurantID,name,type,url) values,
+insert into ottawaspoon.restaurant(restaurantID,name,type,url) values,()
 
-insert into ottawaspoon.MenuItem(itemID,name,type,category,description,price,restaurantID) values,
+insert into ottawaspoon.MenuItem(itemID,name,type,category,description,price,restaurantID) values,()
 
 --delete the data into the rater/restaurant/menuItem
+
+delete from rater where rater.userID = '1'
+
+delete from restaurant where restaurant.restaurantID = 1
+
+delete from menuItem where menuItem.itemID = 1
 
